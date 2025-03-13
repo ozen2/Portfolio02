b81b2754c6b94914f55e9a3e33b93ac4ff80ae19
Enzo Michel <enzomichel399@gmail.com>
2025-03-13 21:41:15 +0100

    dkfj

--- src/app/components/bento/Bento.jsx
+++ src/app/components/bento/Bento.jsx
@@ -6,43 +6,48 @@ import nebula from "../../../../public/images/projets/nebula.jpg";
 
 export default function Bento() {
   return (
-    <div className="parent mx-5 mt-32">
-      <div className="div1 relative">
-        {" "}
+    <div className="parent mx-5 mt-32 custom-cursor">
+      <div className="div1 relative overflow-hidden">
+        <div className="bg-black w-full h-full"></div>
         <Image
           src={externatic}
           alt="mairieArcins"
           fill
           unoptimized={true}
-          className="object-cover w-full h-full"
+          className="object-cover transition-all duration-500 hover:scale-105 ease-in-out hover:opacity-80 w-full custom-cursor h-full"
         />
+        <h2 className="text-white absolute bottom-10 left-10 text-3xl Switzer-normal opacity-O">
+          Externatic
+        </h2>
       </div>
-      <div className="div2 relative">
-        {" "}
+      <div className="div2 relative overflow-hidden">
+        <div className="bg-black w-full h-full"></div>
         <Image
           src={randoZen}
           alt="mairieArcins"
           fill
           unoptimized={true}
-          className="object-cover w-full h-full"
+          className="object-cover transition-all duration-500 hover:scale-105 ease-in-out hover:opacity-80 w-full custom-cursor h-full"
         />
       </div>
-      <div className="div3 relative">
+      <div className="div3 relative overflow-hidden">
+        <div className="bg-black w-full h-full"></div>
         <Image
           src={nebula}
           alt="mairieArcins"
           fill
           unoptimized={true}
-          className="object-cover w-full h-full"
+          className="object-cover transition-all duration-500 hover:scale-105 ease-in-out hover:opacity-80 w-full custom-cursor h-full"
         />
       </div>
-      <div className="div4 relative">
+      <div className="div4 relative overflow-hidden">
+        <div className="bg-black w-full h-full"></div>
         <Image
           src={mairieArcins}
           alt="mairieArcins"
           fill
           unoptimized={true}
-          className="object-cover w-full h-full"
+          className="object-cover transition-all duration-500 hover:scale-105 ease-in-out hover:opacity-80 w-full custom-cursor h-full"
         />
       </div>
     </div>
